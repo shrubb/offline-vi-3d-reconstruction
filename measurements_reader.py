f315cb5b0bc6113596aa45c23111ef4c4ac92c55
Egor Burkov <shrubb@ya.ru>
2020-03-30 06:04:45 +0300

    Add `camera_intrinsics()` and `get_video_reader()` to `MARSMeasurementsReader`

--- measurements_reader.py
+++ measurements_reader.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import numpy as np
 import numpy.lib.recfunctions
 
+import cv2
+
 NANOSECONDS_IN_SECOND = 10**9
 
 class MARSMeasurementsReader:
@@ -16,7 +18,7 @@ class MARSMeasurementsReader:
             pathlib.Path or str
             A directory with 'frame_timestamps.txt', 'gyro_accel.csv', 'movie_metadata.csv', 'movie.mp4'.
         """
-        path = Path(path)
+        self.path = Path(path)
 
         ##############################       Read IMU data       ##############################
 
@@ -29,7 +31,7 @@ class MARSMeasurementsReader:
             ('Ay', np.float64),
             ('Az', np.float64),
         ])
-        IMU_data = np.loadtxt(path / "gyro_accel.csv", dtype=dtype, delimiter=',', skiprows=1)
+        IMU_data = np.loadtxt(self.path / "gyro_accel.csv", dtype=dtype, delimiter=',', skiprows=1)
 
         # Time in nanoseconds
         self.timestamps_IMU = IMU_data['time']
@@ -44,13 +46,19 @@ class MARSMeasurementsReader:
             ('time', np.int64),
             ('fx', np.float64),
             ('fy', np.float64),
-            ('focal_length', np.float64),
-            ('focus_distance', np.float64),
         ])
-        camera_data = np.loadtxt(path / "movie_metadata.csv", dtype=dtype, delimiter=',', skiprows=1, usecols=(0,1,2,8,9))
+        camera_data = np.loadtxt(self.path / "movie_metadata.csv", dtype=dtype, delimiter=',', skiprows=1, usecols=(0,1,2))
 
-        # Time in nanoseconds (consistent with `self.timestamps_IMU`)
+        # Time in nanoseconds (scale is consistent with `self.timestamps_IMU`, but timestamps aren't)
         self.timestamps_camera = camera_data['time']
+        self.camera_focal_length = np.lib.recfunctions.structured_to_unstructured(camera_data[['fx', 'fy']])
+
+        #############################  Check video for frame size  ##############################
+
+        ok, sample_frame = cv2.VideoCapture(str(self.path / "movie.mp4")).read()
+        assert ok
+        self.DOWNSCALING = 2.0 # not used yet
+        self.IMAGE_SIZE = (sample_frame.shape[1] / self.DOWNSCALING, sample_frame.shape[0] / self.DOWNSCALING)
 
     def get_dt_IMU(self, i):
         """
@@ -66,3 +74,38 @@ class MARSMeasurementsReader:
             return 1e-16
         else:
             return (self.timestamps_IMU[i+1] - self.timestamps_IMU[i]) / NANOSECONDS_IN_SECOND
+
+    def camera_intrinsics(self, i):
+        """
+        i:
+            int
+            Index of the camera frame in `self.timestamps_camera`.
+
+        return:
+            gtsam.Cal3DS2
+        """
+        return gtsam.Cal3DS2(
+            *self.camera_focal_length[i],         # focal length
+            0,                                    # skew
+            IMAGE_SIZE[0] / 2, IMAGE_SIZE[1] / 2, # principal point
+            0.25669783, -1.40364704,              # distortion for Huawei Honor 10: k1, k2
+            -0.00332119, 0.00333635               # distortion for Huawei Honor 10: p1, p2
+        )
+
+    def get_video_reader(self):
+        """
+        return:
+            generator, yields np.ndarray
+        """
+        def generator(video_reader, scaling_factor):
+            while video_reader.grab():
+                ok, image = video_reader.retrieve()
+                assert ok
+                image = cv2.resize(
+                    image, (0, 0), fx=scaling_factor, fy=scaling_factor,
+                    interpolation=cv2.INTER_AREA)
+
+                yield np.rot90(image).copy()
+
+        video_reader = cv2.VideoCapture(str(self.path / "movie.mp4"))
+        return generator(video_reader, 1 / self.DOWNSCALING)
